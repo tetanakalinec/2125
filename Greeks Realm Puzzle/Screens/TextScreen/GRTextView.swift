77112d2ba6eee71d01521f0c677a5bd7a372f26e
Dima Protsak <>
2025-04-23 21:45:42 +0300

    Updated text view

--- Greeks Realm Puzzle/Screens/TextScreen/GRTextView.swift
+++ Greeks Realm Puzzle/Screens/TextScreen/GRTextView.swift
@@ -126,63 +126,60 @@ enum TypeScreen {
         switch self {
         case .terms:
                     """
-                Welcome to our game! By accessing and playing our game, you agree to comply with the following Terms of Use. Please read them carefully.
-
-                1. Acceptance of Terms:
-                - By downloading, installing, or using our game, you agree to be bound by these Terms of Use and our Privacy Policy. If you do not agree with these terms, please do not use the game.
-
-                2. License to Use:
-                - We grant you a limited, non-exclusive, non-transferable, and revocable license to use our game for personal entertainment purposes only. You may not use the game for any commercial purpose without our express permission.
-
-                3. User Conduct:
-                - You agree to use the game in a manner that is lawful and respectful of others. You may not use the game to engage in any illegal activities, distribute harmful software, or harass other players.
-
-                4. Intellectual Property:
-                - All content, features, and functionality of the game, including but not limited to graphics, designs, logos, and software, are the intellectual property of the game developers and are protected by copyright and trademark laws.
-
-                5. Updates and Modifications:
-                - We may update or modify the game at any time without prior notice. These updates may include new features, bug fixes, or other changes to enhance the game. We reserve the right to discontinue or suspend the game at any time.
-
-                6. Limitation of Liability:
-                - We are not responsible for any damages or losses resulting from your use of the game, including but not limited to data loss, device damage, or any other indirect, incidental, or consequential damages.
-
-                7. Termination:
-                - We reserve the right to terminate or suspend your access to the game if you violate these Terms of Use. Upon termination, your right to use the game will cease immediately.
-
-                8. Governing Law:
-                - These Terms of Use are governed by and construed in accordance with the laws of the jurisdiction in which the game developer is based. Any disputes arising from these terms shall be resolved in the courts of that jurisdiction.
-
-                Thank you for playing our game! If you have any questions about these Terms of Use, please contact us through the support section in the game.
+                These terms and conditions apply to the Greeks Realm Puzzle app (hereby referred to as "Application") for mobile devices that was created by Jack Nue (hereby referred to as "Service Provider") as a Free service.
+                Upon downloading or utilizing the Application, you are automatically agreeing to the following terms. It is strongly advised that you thoroughly read and understand these terms prior to using the Application. Unauthorized copying, modification of the Application, any part of the Application, or our trademarks is strictly prohibited. Any attempts to extract the source code of the Application, translate the Application into other languages, or create derivative versions are not permitted. All trademarks, copyrights, database rights, and other intellectual property rights related to the Application remain the property of the Service Provider.
+                The Service Provider is dedicated to ensuring that the Application is as beneficial and efficient as possible. As such, they reserve the right to modify the Application or charge for their services at any time and for any reason. The Service Provider assures you that any charges for the Application or its services will be clearly communicated to you.
+                The Application stores and processes personal data that you have provided to the Service Provider in order to provide the Service. It is your responsibility to maintain the security of your phone and access to the Application. The Service Provider strongly advise against jailbreaking or rooting your phone, which involves removing software restrictions and limitations imposed by the official operating system of your device. Such actions could expose your phone to malware, viruses, malicious programs, compromise your phone's security features, and may result in the Application not functioning correctly or at all.
+                Please be aware that the Service Provider does not assume responsibility for certain aspects. Some functions of the Application require an active internet connection, which can be Wi-Fi or provided by your mobile network provider. The Service Provider cannot be held responsible if the Application does not function at full capacity due to lack of access to Wi-Fi or if you have exhausted your data allowance.
+                If you are using the application outside of a Wi-Fi area, please be aware that your mobile network provider's agreement terms still apply. Consequently, you may incur charges from your mobile provider for data usage during the connection to the application, or other third-party charges. By using the application, you accept responsibility for any such charges, including roaming data charges if you use the application outside of your home territory (i.e., region or country) without disabling data roaming. If you are not the bill payer for the device on which you are using the application, they assume that you have obtained permission from the bill payer.
+                Similarly, the Service Provider cannot always assume responsibility for your usage of the application. For instance, it is your responsibility to ensure that your device remains charged. If your device runs out of battery and you are unable to access the Service, the Service Provider cannot be held responsible.
+                In terms of the Service Provider's responsibility for your use of the application, it is important to note that while they strive to ensure that it is updated and accurate at all times, they do rely on third parties to provide information to them so that they can make it available to you. The Service Provider accepts no liability for any loss, direct or indirect, that you experience as a result of relying entirely on this functionality of the application.
+                The Service Provider may wish to update the application at some point. The application is currently available as per the requirements for the operating system (and for any additional systems they decide to extend the availability of the application to) may change, and you will need to download the updates if you want to continue using the application. The Service Provider does not guarantee that it will always update the application so that it is relevant to you and/or compatible with the particular operating system version installed on your device. However, you agree to always accept updates to the application when offered to you. The Service Provider may also wish to cease providing the application and may terminate its use at any time without providing termination notice to you. Unless they inform you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must cease using the application, and (if necessary) delete it from your device.
+                Changes to These Terms and Conditions
+                The Service Provider may periodically update their Terms and Conditions. Therefore, you are advised to review this page regularly for any changes. The Service Provider will notify you of any changes by posting the new Terms and Conditions on this page.
+                These terms and conditions are effective as of 2025-04-22
+                Contact Us
+                If you have any questions or suggestions about the Terms and Conditions, please do not hesitate to contact the Service Provider at tetanakalinec@gmail.com.
                 """
         case .policy:
                     """
-                We value your privacy and are committed to protecting your personal information. This Privacy Policy explains how we collect, use, and safeguard your data when you play our game.
-
-                1. Information We Collect:
-                - We may collect personal information, such as your name, email address, and other contact details, when you create an account or interact with certain features of the game.
-                - We collect non-personal information, such as gameplay data, device information, and usage statistics, to improve our game and provide a better user experience.
-
-                2. How We Use Your Information:
-                - Your personal information is used to manage your account, provide customer support, and communicate with you about updates or promotions.
-                - Non-personal information is used to analyze game performance, optimize features, and enhance your overall experience.
-
-                3. Sharing Your Information:
-                - We do not share your personal information with third parties except as necessary to provide the services or as required by law.
-                - Non-personal information may be shared with our partners and service providers to help improve the game.
-
-                4. Data Security:
-                - We implement industry-standard security measures to protect your data from unauthorized access, disclosure, or loss.
-                - While we strive to protect your personal information, please note that no security system is completely impenetrable.
-
-                5. Your Rights:
-                - You have the right to access, correct, or delete your personal information at any time. Please contact us if you wish to exercise these rights.
-                - You can opt-out of receiving promotional communications by following the instructions in the messages you receive.
-
-                6. Changes to This Policy:
-                - We may update this Privacy Policy from time to time. We will notify you of any significant changes by posting the new policy in the game or by sending an email.
-
-                By using our game, you agree to the terms of this Privacy Policy. If you have any questions or concerns, please contact us through the support section in the game.
-                """
+                    This privacy policy applies to the Greeks Realm Puzzle app (hereby referred to as "Application") for mobile devices that was created by Jack Nue (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".
+                    Information Collection and Use
+                    The Application collects information when you download and use it. This information may include information such as
+                    Your device's Internet Protocol address (e.g. IP address)
+                    The pages of the Application that you visit, the time and date of your visit, the time spent on those pages
+                    The time spent on the Application
+                    The operating system you use on your mobile device
+                    The Application does not gather precise information about the location of your mobile device.
+                    The Application collects your device's location, which helps the Service Provider determine your approximate geographical location and make use of in below ways:
+                    Geolocation Services: The Service Provider utilizes location data to provide features such as personalized content, relevant recommendations, and location-based services.
+                    Analytics and Improvements: Aggregated and anonymized location data helps the Service Provider to analyze user behavior, identify trends, and improve the overall performance and functionality of the Application.
+                    Third-Party Services: Periodically, the Service Provider may transmit anonymized location data to external services. These services assist them in enhancing the Application and optimizing their offerings.
+                    The Service Provider may use the information you provided to contact you from time to time to provide you with important information, required notices and marketing promotions.
+                    For a better experience, while using the Application, the Service Provider may require you to provide us with certain personally identifiable information. The information that the Service Provider request will be retained by them and used as described in this privacy policy.
+                    Third Party Access
+                    Only aggregated, anonymized data is periodically transmitted to external services to aid the Service Provider in improving the Application and their service. The Service Provider may share your information with third parties in the ways that are described in this privacy statement.
+                    The Service Provider may disclose User Provided and Automatically Collected Information:
+                    as required by law, such as to comply with a subpoena, or similar legal process;
+                    when they believe in good faith that disclosure is necessary to protect their rights, protect your safety or the safety of others, investigate fraud, or respond to a government request;
+                    with their trusted services providers who work on their behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this privacy statement.
+                    Opt-Out Rights
+                    You can stop all collection of information by the Application easily by uninstalling it. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network.
+                    Data Retention Policy
+                    The Service Provider will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. If you'd like them to delete User Provided Data that you have provided via the Application, please contact them at tetanakalinec@gmail.com and they will respond in a reasonable time.
+                    Children
+                    The Service Provider does not use the Application to knowingly solicit data from or market to children under the age of 13.
+                    The Service Provider does not knowingly collect personally identifiable information from children. The Service Provider encourages all children to never submit any personally identifiable information through the Application and/or Services. The Service Provider encourage parents and legal guardians to monitor their children's Internet usage and to help enforce this Policy by instructing their children never to provide personally identifiable information through the Application and/or Services without their permission. If you have reason to believe that a child has provided personally identifiable information to the Service Provider through the Application and/or Services, please contact the Service Provider (tetanakalinec@gmail.com) so that they will be able to take the necessary actions. You must also be at least 16 years of age to consent to the processing of your personally identifiable information in your country (in some countries we may allow your parent or guardian to do so on your behalf).
+                    Security
+                    The Service Provider is concerned about safeguarding the confidentiality of your information. The Service Provider provides physical, electronic, and procedural safeguards to protect information the Service Provider processes and maintains.
+                    Changes
+                    This Privacy Policy may be updated from time to time for any reason. The Service Provider will notify you of any changes to the Privacy Policy by updating this page with the new Privacy Policy. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.
+                    This privacy policy is effective as of 2025-04-22
+                    Your Consent
+                    By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy now and as amended by us.
+                    Contact Us
+                    If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at tetanakalinec@gmail.com.
+                    """
         }
     }
 }
